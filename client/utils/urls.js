80957b2e52231ea2b117d9a60f047e6fc0e6c2af
Dan Actor <danltactor@gmail.com>
2018-02-11 21:26:00 -0700

    move expo app to client

---
+++ client/utils/urls.js
@@ -0,0 +1,5 @@
+const BASE = "http://localhost:3001";
+// const BASE = "https://dpl-code-on-yahtzee-server.heroku.com";
+
+export const AUTH_URL = `${BASE}/auth`;
+export const API_URL = `${BASE}/api`;
