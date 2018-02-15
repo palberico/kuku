1610b10e1990743b75e641c7cd9ec45e4ffa67d3
Dan Actor <danltactor@gmail.com>
2018-02-14 19:15:40 -0700

    indentation

--- client/App.js
+++ client/App.js
@@ -29,30 +29,27 @@ import Description from './components/Description';
 
 class App extends Component {
 
-  
   render() {
     console.disableYellowBox = true;
     return (
       <Provider store={store}>
-
-            <NativeRouter>
-              <Switch>
-                <Route exact path="/" component={Home}  />
-                <Route exact path="/faq" component={Faq} />
-                <Route exact path="/shop" component={Shop} />
-                <Route exact path="/cart/:category" component={Cart} />
-                <Route exact path="/custom/:category" component={Custom} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/terms" component={Terms} />
-                <Route exact path="/search" component={Search} />
-                <Route exact path="/card" component={CardComp}  />
-                <Route exact path="/privacy" component={Privacy} />
-                <Route exact path="/settings" component={Settings} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/description/:title/:category/:fromComponent" component={Description} />
-              </Switch>
-            </NativeRouter>
-
+        <NativeRouter>
+          <Switch>
+            <Route exact path="/" component={Home}  />
+            <Route exact path="/faq" component={Faq} />
+            <Route exact path="/shop" component={Shop} />
+            <Route exact path="/cart/:category" component={Cart} />
+            <Route exact path="/custom/:category" component={Custom} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/terms" component={Terms} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/card" component={CardComp}  />
+            <Route exact path="/privacy" component={Privacy} />
+            <Route exact path="/settings" component={Settings} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/description/:title/:category/:fromComponent" component={Description} />
+          </Switch>
+        </NativeRouter>
       </Provider>
     );
   }
