ac90d8ac1465d7de6ad27ef0e881a987b35f76cc
Dan Actor <danltactor@gmail.com>
2018-07-01 17:31:32 -0600

    rails backend, check utils urls

--- client/actions/products.js
+++ client/actions/products.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 import { API_URL } from '../utils/urls';
 
 export const fetchProducts = () => dispatch => {
-  return axios.get(`${API_URL}/products`)
+  return axios.get('https://kukudb-ff7f7.firebaseio.com/products.json')
     .then( res => {
       let unseenItems = res.data.slice(0);
-      axios.put('/api/products', unseenItems)
-      // axios.delete('https://kukudb-ff7f7.firebaseio.com/cart.json')
-      // axios.delete('https://kukudb-ff7f7.firebaseio.com/dislike.json')
+      axios.put('https://kukudb-ff7f7.firebaseio.com/unseen_items.json', unseenItems)
+      axios.delete('https://kukudb-ff7f7.firebaseio.com/cart.json')
+      axios.delete('https://kukudb-ff7f7.firebaseio.com/dislike.json')
       dispatch({ type: 'FETCH_PRODUCTS', products: res.data })
     })
 };
--- client/utils/urls.js
+++ client/utils/urls.js
@@ -1,5 +1,4 @@
 const BASE = "http://localhost:3001";
-// const BASE = "https://dpl-code-on-yahtzee-server.heroku.com";
 
 export const AUTH_URL = `${BASE}/auth`;
 export const API_URL = `${BASE}/api`;
